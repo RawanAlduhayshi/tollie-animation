e7f03bc4b8610808552d7c13ac789627a0c276e8
Rawan <Rawan.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-12-13 14:50:18 +0300

    tollie animation

---
+++ app/src/main/java/com/example/tollieanimation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.tollieanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+////   val animation = findViewById<LottieAnimationView>()
+////        with(animation){
+////        setMinAndMaxFrame(30,50)
+////        }
+//
+//    }
+    }
+}
